604d4f7a573ba290c02c2071da3efe083fc65d69
johanna071101 <johanna.weigl@t-online.de>
2026-04-29 11:59:26 +0200

    j

--- 04/script.js
+++ 04/script.js
@@ -9,14 +9,14 @@ setInterval(() => {
     //Bild zeichnen
     
         console.log(" -\\  /- \n|  \\/  |\n \\    / \n  \\  /  \n   \\/   ");
-        love= love +1;
+        
     
-    if(love > 3) {
+    
         console.clear();
         console.log(" -\\   /- \n|  \\ /  |\n \\ / / / \n  \\\\ \\/  \n   \\ /   ");
-        love = 0;
         
-    }
+        
+    
     
 
 }, 400); // Alle 200ms ausführen
